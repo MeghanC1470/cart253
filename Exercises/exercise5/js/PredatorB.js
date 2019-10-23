f8b8f1db211629b485bbb4a51e4e8489e6707525
Meghan <MeghanC2000@outlook.com>
2019-10-22 21:05:38 -0400

    Ex5: Complete

--- Exercises/exercise5/js/PredatorB.js
+++ Exercises/exercise5/js/PredatorB.js
@@ -2,7 +2,7 @@ class PredatorB {
 
   // constructor
   //
-  // Sets the initial values for the Predator's properties
+  // Sets the initial values for the PredatorB's properties
   // Either sets default values or uses the arguments provided
   constructor(x, y, speed, fillColor, radius) {
     // Position

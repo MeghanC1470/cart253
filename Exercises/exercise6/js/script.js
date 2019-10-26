8847df47f15deb1ecf87fe985e9b24623b299e82
Meghan <MeghanC2000@outlook.com>
2019-10-26 14:01:36 -0400

    Ex6: Missing Y Fixed
    Missing Y value in Zebra = new Prey in SetUp. 5 Errors Remaining

--- Exercises/exercise6/js/script.js
+++ Exercises/exercise6/js/script.js
@@ -8,7 +8,7 @@
 // The predator loses health over time, so must keep eating to survive.
 
 ////////////////////
-// 7 ERRORS IN HERE
+// 5 ERRORS IN HERE
 ////////////////////
 
 // Our predator
@@ -27,7 +27,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
-  zebra = new Prey(100, 8, color(255, 255, 255), 60);
+  zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
 

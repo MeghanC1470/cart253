5f3aea525b8c81e8251a0584bd0abd08d5abfe6f
Meghan <MeghanC2000@outlook.com>
2019-10-26 15:20:33 -0400

    Ex6: < Fixed
    Fixed a behavioural problem involving the bracket in (his.x > 0). 2 Errors remaining

--- Exercises/exercise6/js/Prey.js
+++ Exercises/exercise6/js/Prey.js
@@ -5,7 +5,7 @@
 // the screen and be consumed by Predator objects.
 
 /////////////////////////
-// 3 ERRORS IN HERE
+// 2 ERRORS IN HERE
 /////////////////////////
 
 //////////////// FIXED
@@ -62,7 +62,8 @@ class Prey {
     // wraps it to the other side if so
     handleWrapping() {
       // Off the left or right
-      if (this.x > 0) {
+      //////////////// FIXED
+      if (this.x < 0) {
         this.x += width;
       }
       else if (this.x > width) {

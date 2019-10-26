1ccdccdf4c81b20adc32f9af7ae321f0471865b1
Meghan <MeghanC2000@outlook.com>
2019-10-26 14:39:18 -0400

    Ex6: Misspleted Display Fixed
    Fixed the spelling in zebra.display();. 1 Error Remaining
    + Added all ////// FIXED comments

--- Exercises/exercise6/js/script.js
+++ Exercises/exercise6/js/script.js
@@ -8,13 +8,14 @@
 // The predator loses health over time, so must keep eating to survive.
 
 ////////////////////
-// 2 ERRORS IN HERE
+// 1 ERROR IN HERE
 ////////////////////
 
 // Our predator
 let tiger;
 
 // The three prey
+//////////////// FIXED
 let antelope;
 let zebra;
 let bee;
@@ -23,10 +24,13 @@ let bee;
 //
 // Sets up a canvas
 // Creates objects for the predator and three prey
+//////////////// FIXED
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //////////////// FIXED
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
+  //////////////// FIXED
   zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
@@ -36,15 +40,18 @@ function setup() {
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
   // Clear the background to black
+  //////////////// FIXED
   background(0);
 
   // Handle input for the tiger
+  //////////////// FIXED
   tiger.handleInput();
 
   // Move all the "animals"
   tiger.move();
   antelope.move();
   zebra.move();
+  //////////////// FIXED
   bee.move();
 
   // Handle the tiger eating any of the prey
@@ -54,7 +61,8 @@ function draw() {
 
   // Display all the "animals"
   tiger.display();
+  //////////////// FIXED
   antelope.display();
-  zebra.disploy();
+  zebra.display();
   b.display();
 }

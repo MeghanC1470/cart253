9f32a597d718449a27c5da5b0ef93ee50864b2d0
Meghan <MeghanC2000@outlook.com>
2019-10-26 15:04:56 -0400

    Ex6: Move(); Fixed
    Fixed a spelling error in the Move Function. 7 Errors remaining

--- Exercises/exercise6/js/Prey.js
+++ Exercises/exercise6/js/Prey.js
@@ -5,7 +5,7 @@
 // the screen and be consumed by Predator objects.
 
 /////////////////////////
-// 8 ERRORS IN HERE
+// 7 ERRORS IN HERE
 /////////////////////////
 
 //////////////// FIXED
@@ -39,7 +39,7 @@ class Prey {
   //
   // Sets velocity based on the noise() function and the Prey's speed
   // Moves based on the resulting velocity and handles wrapping
-  mover() {
+  move() {
     // Set velocity via noise()
     this.vx = map(noise(this.tx), 0, 1, -this.speed, this.speed);
     this.vy = map(noise(this.ty), 0, 1, -this.speed, this.speed);
